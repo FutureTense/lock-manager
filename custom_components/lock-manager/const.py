9c5c53f43613b33335a05c3ac20e3f7b8952ba13
firstof9 <firstof9@gmail.com>
2020-08-10 15:25:14 -0700

    Refactoring for better UX
    * Added Sensors Lists
    * Sensors are populated with relevant sensors
    * Adjust template to no longer require renaming entity_ids
    * Added template information for 'access_control' and 'user_code' sensors
    * Commented out template code that should be user configured in their configs ie: binary_sensors for doors
    * Commented out the notify script, user should make their own script that takes the data we're outputting so they can send it to what ever service they like

--- custom_components/lock-manager/const.py
+++ custom_components/lock-manager/const.py
@@ -1,11 +1,12 @@
 DOMAIN = "lock-manager"
-VERSION = "0.0.4"
-ISSUE_URL = "https://github.com/firstof9/lock-manager"
+VERSION = "0.0.5"
+ISSUE_URL = "https://github.com/FutureTense/lock-manager"
 
 # Configuration Properties
+CONF_ALARM_LEVEL = "alarm_level"
+CONF_ALARM_TYPE = "alarm_type"
 CONF_ENTITY_ID = "entity_id"
 CONF_PATH = "packages_path"
-CONF_ENTITY_ID = "entity_id"
 CONF_LOCK_NAME = "lockname"
 CONF_SENSOR_NAME = "sensorname"
 CONF_SLOTS = "slots"
